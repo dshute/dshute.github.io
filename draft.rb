4c2e29ee365224ea0ee5ed9e8ffffe42828622af
David Shute <dshute@gmail.com>
2018-04-03 21:44:25 -0400

    updated draft.rb to support multiple files and be user driven instaed of command line arg driven

--- draft.rb
+++ draft.rb
@@ -5,54 +5,99 @@
 # simple tool to generate drafts and then push them to post
 # mostly used to avoid naming files and updating metadata
 #
-# post grabs the first thing it can find and moves that
-# will not work for managing multiple files
-# will automatically grab the first results for post
+# rewrite includes support for
+#		* list existing drafts
+# 		* create a new draft
+#		* publish from list of existing drafts
 
-def BuildPost(args)
-  postFile = args[1] + ".markdown"
+def displayMenu()
+	puts
+	puts "1. create new"
+	puts "2. publish existing"
+	puts "3. list existing"
+end
+
+def listFiles()
+	files = Dir.entries("_drafts")
+	files.delete(".")
+	files.delete("..")
+	puts
+	files.each { |file|
+		puts file
+	}
+end
+
+def newDraft()
+	puts
+	print "\n\nfilename: "
+	filename = gets.chomp
+
+	File.open("_drafts/#{filename}.markdown", "w")
+	puts "File created: #{filename}.markdown"
+end
+
+def publishPost()
+	listFiles
+	puts
+	print "Filename: "
+	postFile = gets.chomp
+	postFile += ".markdown"
 
   if File.exist?("_drafts/#{postFile}")
-    newFile = "_posts/" + Time.now.strftime("%Y-%m-%d-") + postFile
-    oldFile = File.read("_drafts/#{postFile}")
-    File.open(newFile, "w") { |file|
-      file.puts "---"
-      file.puts "layout: post"
-      file.puts "title: #{args[2]}"
-      file.puts "date:   #{Time.now.strftime("%Y-%m-%d %H:%M:%S")}"
-      file.puts "categories: #{args[3]}"
-      file.puts "---"
-      file.puts oldFile
-    }
-    File.delete("_drafts/#{postFile}")
+		# get metadata
+		print "Title: "
+		postTitle = gets.chomp
+		print "Category: "
+		postCategory = gets.chomp
+
+		puts
+		puts postFile
+		puts postTitle
+		puts postCategory
+		print "\nIs this correct? "
+		verify = gets.chomp
+		if (verify == "y")
+			# write new file
+	    newFile = "_posts/" + Time.now.strftime("%Y-%m-%d-") + postFile
+	    oldFile = File.read("_drafts/#{postFile}")
+	    File.open(newFile, "w") { |file|
+	      file.puts "---"
+	      file.puts "layout: post"
+
+	      file.puts "title: #{postTitle}"
+	      file.puts "date:   #{Time.now.strftime("%Y-%m-%d %H:%M:%S")}"
+	      file.puts "categories: #{postCategory}"
+	      file.puts "---"
+	      file.puts oldFile
+	    }
+	    File.delete("_drafts/#{postFile}")
+		else
+			puts
+			puts "aborted move of #{postFile} from draft to post"
+		end
   else
-    puts "\n\tNo draft to post names #{postFile}"
-    puts "\t create new draft - draft new {filename}"
+    puts "\nInvalid filename -- #{postFile}"
   end
 end
 
-def ListDrafts()
-  files = Dir.entries("_drafts")
-  files.each { |file|
-    puts file unless file == "." || file == ".."
-  }
-end
 
-case ARGV[0]
-when "new"
-  unless ARGV[1] == nil
-    puts "\n\tNew draft created - _drafts/#{ARGV[1]}.markdown"
-    File.open("_drafts/#{ARGV[1]}.markdown", "w")
-  else
-    puts "\n\tdraft new {filename}"
-  end
-when "list"
-  ListDrafts()
-when "post"
-  BuildPost(ARGV) unless ARGV.length < 4
-else
-  puts "\n\tdraft new {filename} - creates a new draft with given title"
-  puts "\n\tdraft post {filename} {title} {tag} - updates and moves draft to posts"
-end
+running = true
 
-puts
+while(running)
+	displayMenu
+	user_command = gets.chomp
+
+	case user_command
+	when "1"
+			newDraft
+	when "2"
+			publishPost
+	when "3"
+			listFiles
+	when "q", "Q"
+		running = false
+	else
+		puts
+		puts
+	end
+end
